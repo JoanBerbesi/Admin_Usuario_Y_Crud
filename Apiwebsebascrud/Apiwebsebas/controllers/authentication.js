e50514ddd6e49630b51f461712bcd0270ef3d864
JoanBerbesi <163789478+JoanBerbesi@users.noreply.github.com>
2024-06-04 02:52:29 -0500

    Update authentication.js

--- Apiwebsebascrud/Apiwebsebas/controllers/authentication.js
+++ Apiwebsebascrud/Apiwebsebas/controllers/authentication.js
@@ -16,7 +16,7 @@ if (loginin) {
 
     if(validation != null){
         alert('Authentication sucessfull '+email)
-        window.location.href='/templates/pagina.html';
+        window.location.href='./templates/pagina.html';
         await displayUserData();
     }
     else{
@@ -37,7 +37,7 @@ if (googleLoginBtn) {
       const result = await signInWithGoogle();
       const user = result.user;
       alert('Authentication successful ' + user.email);
-      window.location.href='/templates/pagina.html';
+      window.location.href='./templates/pagina.html';
     } catch (error) {
       alert('Error authentication not successful');
       console.log('session not validated');
@@ -51,7 +51,7 @@ if (facebookLoginBtn) {
       const result = await signInWithFacebook();
       const user = result.user;
       alert('Authentication successful ' + user.email);
-      window.location.href='/templates/pagina.html';
+      window.location.href='./templates/pagina.html';
     } catch (error) {
       alert('Error authentication not successful');
       console.log('session not validated');
@@ -90,7 +90,7 @@ if (registerBtn) {
       const result = await register(email, password);
       const user = result.user;
       alert('Registration successful. A verification email has been sent to ' + user.email);
-      window.location.href='/templates/pagina.html';
+      window.location.href='./templates/pagina.html';
     } catch (error) {
       alert('Error registration not successful');
       console.log('registration not validated');
@@ -110,7 +110,7 @@ window.addEventListener('DOMContentLoaded', () => {
       try {
         await sendResetEmail(email);
         alert('Se ha enviado un correo de restablecimiento de contraseña a ' + email);
-        window.location.href="/index.html";
+        window.location.href="./index.html";
       } catch (error) {
         alert('Error al enviar el correo de restablecimiento de contraseña');
         console.log('Error al enviar el correo de restablecimiento de contraseña: ', error);
